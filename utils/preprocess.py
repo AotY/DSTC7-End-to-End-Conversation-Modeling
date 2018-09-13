b190ec7f456589bd225a87020430ecfedb072075
AotY <694731929@qq.com>
2018-09-13 18:53:26 +0800

    vocab

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -27,6 +27,8 @@ tokenizer = Tokenize()
 '''
 Read convos file.
 '''
+
+
 def read_convos(convos_file_path, logger=None):
     with open(convos_file_path, 'r', encoding='utf-8') as f:
         lines = f.readlines()
@@ -67,9 +69,12 @@ def read_convos(convos_file_path, logger=None):
 
     return conversations, responses, conversation_max_length, response_max_length
 
+
 '''
 Read facts file.
 '''
+
+
 def read_facts(facts_file_path, logger):
     with open(facts_file_path, 'r', encoding='utf-8') as f:
         lines = f.readlines()
@@ -104,7 +109,8 @@ Statistical frequency
 datas, may be conversations + responses or conversations individually.
 '''
 
-def stat_frequency(datas, datas_name, min_count=None, max_vocab_size=None, logger=None):
+
+def stat_frequency(datas, datas_name, min_count=3, max_vocab_size=8e5, logger=None):
     freq_dict = {}
 
     for data in datas:
@@ -115,12 +121,11 @@ def stat_frequency(datas, datas_name, min_count=None, max_vocab_size=None, logge
     sorted_freq_list = sorted(
         freq_dict.items(), key=lambda d: d[1], reverse=True)
 
-    if min_count is not None and min_count > 0:
+    if min_count > 0:
         logger.info('Clip tokens by min_count')
         sorted_freq_list = [item for item in sorted_freq_list if item[1] > min_count]
 
-
-    if max_vocab_size is not None and len(sorted_freq_list) > max_vocab_size:
+    if max_vocab_size > 0 and len(sorted_freq_list) > max_vocab_size:
         logger.info('Clip tokens by max_vocab_size')
         sorted_freq_list = sorted_freq_list[:max_vocab_size]
 
@@ -134,18 +139,16 @@ def stat_frequency(datas, datas_name, min_count=None, max_vocab_size=None, logge
     return sorted_freq_list
 
 
-
-
 '''
 build vocab
 '''
+
 
 def build_vocab(freq_list):
     vocab = Vocab()
     vocab.build_for_frequency(freq_list)
     vocab.save()
     return vocab
-
 
 
 def generate_num(datas, vocab, save_path):
@@ -170,28 +173,27 @@ if __name__ == '__main__':
     preprocess_opt(parser)
     opt = parser.parse_args()
 
+    logger.info('opt.max_vocab_size: %f ' % opt.max_vocab_size) \
+ \
     conversations, responses, conversation_max_length, response_max_length = read_convos(opt.convos_file_path, logger)
-    logger.info('conversation_max_length: %d ' % conversation_max_length)
-    logger.info('response_max_length: %d ' % response_max_length)
+    logger.info('conversation_max_length: %d ' % conversation_max_length)  # 2429
+    logger.info('response_max_length: %d ' % response_max_length)  # 186
 
-    # # facts = read_facts(opt.facts_file_path)
-    #
-    # stat_frequency(conversations, ['conversations'], None, None, logger)
-    # stat_frequency(responses, ['responses'], None, None, logger)
-    #
-    # datas = conversations + responses
-    # datas_name = ['conversations', 'responses']
-    # sorted_freq_list = stat_frequency(datas, datas_name, opt.min_count, opt.max_vocab_size, logger)
-    #
-    # vocab = build_vocab(sorted_freq_list)
-    vocab = Vocab()
-    vocab.load()
-    logger.info('vocab_size: %s' % vocab.get_vocab_size())
+    # facts = read_facts(opt.facts_file_path)
+
+    stat_frequency(conversations, ['conversations'], 0, 0, logger)
+    stat_frequency(responses, ['responses'], 0, 0, logger)
+
+    datas = conversations + responses
+    datas_name = ['conversations', 'responses']
+    sorted_freq_list = stat_frequency(datas, datas_name, opt.min_count, opt.max_vocab_size, logger)
+
+    vocab = build_vocab(sorted_freq_list)
+    # vocab = Vocab()
+    # vocab.load()
+    logger.info('vocab_size: %s' % vocab.get_vocab_size())  # 93423
 
     generate_num(conversations, vocab, opt.conversations_num_save_path)
     generate_num(responses, vocab, opt.responses_num_save_path)
 
     logger.info('Preprocess finished.')
-
-
-
