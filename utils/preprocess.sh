36d8e0dc440d03d0d70758724bc817335f8f5601
AotY <694731929@qq.com>
2018-09-14 17:45:16 +0800

    full preprocess

--- utils/preprocess.sh
+++ utils/preprocess.sh
@@ -3,8 +3,8 @@
 python preprocess.py \
     --convos_file_path ./../data/train.convos.txt \
     --facts_file_path ./../data/train.facts.txt \
-    --conversations_num_save_path ./../data/train.convos.num.txt \
-    --responses_num_save_path ./../data/train.facts.num.txt \
+    --conversations_num_save_path ./../data/train.conversations.num.txt \
+    --responses_num_save_path ./../data/train.responses.num.txt \
     --save_path ./../data \
     --min_count 3 \
     --max_vocab_size 8e4 \
