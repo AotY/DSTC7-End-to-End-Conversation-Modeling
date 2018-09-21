7ec67b569c55b1cba5378c1abe3c20c253a6fb79
AotY <694731929@qq.com>
2018-09-21 15:41:24 +0800

    embeddign

--- modules/Embeddings.py
+++ modules/Embeddings.py
@@ -2,11 +2,10 @@
 from __future__ import division
 from __future__ import print_function
 
+import math
 import torch
 import torch.nn as nn
 from torch.autograd import Variable
-import math
-
 from modules.utils import aeq
 
 
@@ -56,43 +55,46 @@ class Embeddings(nn.Module):
           A-->B[Word Lookup]
           A-->C[Output]
     Args:
-        word_vec_size (int): size of the dictionary of embeddings.
-        word_padding_idx (int): padding index for words in the embeddings.
-        word_vocab_size (int): size of dictionary of embeddings for words.
+        embeddign_dim (int): size of the dictionary of embeddings.
+        padding_idx (int): padding index for words in the embeddings.
+        vocab_size (int): size of dictionary of embeddings for words.
         dropout (float): dropout probability.
     """
 
     def __init__(self,
-                 word_vec_size,
-                 word_vocab_size,
-                 word_padding_idx,
+                 embeddign_dim,
+                 vocab_size,
+                 padding_idx,
                  dropout_ratio=0.0,
                  sparse=False):
 
         super(Embeddings, self).__init__()
-        self.word_padding_idx = word_padding_idx
+        self.padding_idx = padding_idx
 
         # Dimensions and padding for constructing the word embedding matrix
-        self.vocab_size = word_vocab_size
-        self.emb_dim = word_vec_size
-        self.pad_indice = word_padding_idx
+        self.vocab_size = vocab_size
 
-        # The embedding matrix look-up tables. The first look-up table
-        # is for words. Subsequent ones are for features, if any exist.
-        self.embeddings = nn.Embedding(self.vocab_size, self.emb_dim,
-                                       padding_idx=self.pad_indice, sparse=sparse)
         # This is the attribute you should access if you need to know
         # how big your embeddings are going to be.
         self.embedding_size = self.emb_dim
+
+        self.pad_indice = padding_idx
+
+        # The embedding matrix look-up tables. The first look-up table
+        # is for words. Subsequent ones are for features, if any exist.
+        self.embeddings = nn.Embedding(self.vocab_size, self.embedding_size,
+                                       padding_idx=self.pad_indice, sparse=sparse)
+
 
         # The sequence of operations that converts the input sequence
         # into a sequence of embeddings. At minimum this consists of
         # looking up the embeddings for each word and feature in the
         # input. Model parameters may require the sequence to contain
         # additional operations as well.
+        if dropout_ratio != 0.0:
+            self.dropout = nn.Dropout(p=dropout_ratio)
 
-        self.dropout = nn.Dropout(p=dropout_ratio)
-
+    '''
     @property
     def word_lut(self):
         return self.embeddings
@@ -100,34 +102,37 @@ class Embeddings(nn.Module):
     @property
     def emb_luts(self):
         return self.embeddings
+    '''
+    def get_lookup_table(self):
+        return self.embeddings
 
-    def load_pretrained_vectors(self, emb_file, fixed):
-        """Load in pretrained embeddings.
+    def set_pretrained_embeddings(self, pre_trained_weight, fixed):
+        """Set pretrained embeddings.
         Args:
-          emb_file (str) : path to torch serialized embeddings
+          pre_trained_weight (str) : path to torch serialized embeddings
           fixed (bool) : if true, embeddings are not updated
         """
-        if emb_file:
-            pretrained = torch.load(emb_file)
-            self.embeddings.weight.data.copy_(pretrained)
+        if pre_trained_weight:
+            if !isinstance(pre_trained_weight, torch.Tensor):
+                pre_trained_weight = torch.from_numpy(pre_trained_weight)
+            self.embeddings.weight.data.copy_(pre_trained_weight)
             if fixed:
                 self.embeddings.weight.requires_grad = False
 
-    def forward(self, input):
+    def forward(self, inputs):
         """
         Computes the embeddings for words and features.
         Args:
-            input (`LongTensor`): index tensor `[len x batch]`
+            inputs (`LongTensor`): index tensor `[len x batch]`
         Return:
             `FloatTensor`: word embeddings `[len x batch x embedding_size]`
         """
-        in_length, in_batch = input.size()
-        print("in_length: {}", in_length)
-        print("in_batch: {}", in_batch)
+        in_length, in_batch = inputs.size()
+        print("Embedding inputs shape: {}", inputs.shape)
 
         # aeq(nfeat, len(self.emb_luts))
 
-        emb = self.embeddings(input)
+        emb = self.embeddings(inputs)
         emb = self.dropout(emb)
 
         out_length, out_batch, emb_size = emb.size()
