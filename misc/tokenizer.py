de715708d4a05cc8a58010146dcee10de0ebb6ec
AotY <694731929@qq.com>
2018-09-27 15:45:57 +0800

    preprocess

--- misc/tokenizer.py
+++ misc/tokenizer.py
@@ -21,7 +21,8 @@ class Tokenizer:
             r'<[^>]+>',  # HTML tags
             r'(?:@[\w_]+)',  # @-mentions
             r"(?:\#+[\w_]+[\w\'_\-]*[\w_]+)",  # hash-tags
-            r'http[s]?://(?:[a-z]|[0-9]|[$-_@.&amp;+]|[!*\(\),]|(?:%[0-9a-f][0-9a-f]))+',  # URLs
+            # URLs
+            r'http[s]?://(?:[a-z]|[0-9]|[$-_@.&amp;+]|[!*\(\),]|(?:%[0-9a-f][0-9a-f]))+',
 
             r'(?:(?:\d+,?)+(?:\.?\d+)?)',  # numbers
             r"(?:[a-z][a-z'\-_]+[a-z])",  # words with - and '
@@ -29,8 +30,20 @@ class Tokenizer:
             r'(?:\S)'  # anything else
         ]
 
-        self.tokens_re = re.compile(r'(' + '|'.join(regex_str) + ')', re.VERBOSE | re.IGNORECASE)
-        self.emoticon_re = re.compile(r'^' + emoticons_str + '$', re.VERBOSE | re.IGNORECASE)
+        # URLs
+        url_regex_str = r'http[s]?://(?:[a-z]|[0-9]|[$-_@.&amp;+]|[!*\(\),]|(?:%[0-9a-f][0-9a-f]))+'
+
+        self.url_re = re.compile(url_regex_str, re.VERBOSE | re.IGNORECASE)
+
+        self.tokens_re = re.compile(
+            r'(' + '|'.join(regex_str) + ')', re.VERBOSE | re.IGNORECASE)
+        self.emoticon_re = re.compile(
+            r'^' + emoticons_str + '$', re.VERBOSE | re.IGNORECASE)
+
+    ''' replace url by URL_TAG'''
+
+    def replace_url(self, tokens):
+        return ['URL_TAG' if self.url_re.search(token) else token for token in tokens]
 
     def tokenize(self, s):
         return self.tokens_re.findall(s)
@@ -38,7 +51,8 @@ class Tokenizer:
     def preprocess(self, s, lowercase=False):
         tokens = self.tokenize(s)
         if lowercase:
-            tokens = [token if self.emoticon_re.search(token) else token.lower() for token in tokens]
+            tokens = [token if self.emoticon_re.search(
+                token) else token.lower() for token in tokens]
         return tokens
 
 
