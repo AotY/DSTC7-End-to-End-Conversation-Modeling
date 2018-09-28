0384d5136993799f00bcd4649957004b75ecc235
AotY <694731929@qq.com>
2018-09-28 15:46:07 +0800

    logger

--- train_seq2seq_model.sh
+++ train_seq2seq_model.sh
@@ -35,7 +35,7 @@ python train_seq2seq_model.py \
     --seed 7 \
     --device cuda \
     --log_interval 100 \
-    --log_file ./logs/train_seq2seq_model.log
+    --log_file ./logs/train_seq2seq_model.log \
     --model_save_path ./models \
     --test_split 0.2 \
     --optim_method adam \
