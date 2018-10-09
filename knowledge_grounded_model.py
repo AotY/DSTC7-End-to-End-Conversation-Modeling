e99c75d099bfb43fd07dd5d37c39248586dca949
AotY <694731929@qq.com>
2018-10-09 17:28:13 +0800

    kg, seq2seq

--- knowledge_grounded_model.py
+++ knowledge_grounded_model.py
@@ -8,8 +8,16 @@ import torch.nn as nn
 from modules.encoder import RNNEncoder
 from modules.decoder import StdRNNDecoder
 
+"""
+KnowledgeGroundedModel
+1. dialog_encoder
+2. facts_encoder
+3. dialog_decoder
 
-class Seq2SeqModel(nn.Module):
+"""
+
+
+class KnowledgeGroundedModel(nn.Module):
     '''
     Knowledge-ground model
     Conditoning responses on both conversation history and external "facts", allowing the model
@@ -24,12 +32,15 @@ class Seq2SeqModel(nn.Module):
                  dialog_encoder_rnn_type='LSTM',
                  dialog_encoder_dropout_rate=0.5,
                  dialog_encoder_max_length=32,
-                 dialog_encoder_clipnorm=1.0,
-                 dialog_encoder_clipvalue=0.5,
+                 dialog_encoder_clipnorm=50.0,
                  dialog_encoder_bidirectional=True,
                  dialog_encoder_embedding=None,
                  dialog_encoder_pad_id=0,
-                 dialog_encoder_tied=True,
+
+                 facts_embedding_size=300,
+                 facts_vocab_size=None,
+                 facts_dropout_rate=0.5,
+                 facts_max_length=32,
 
                  dialog_decoder_embedding_size=300,
                  dialog_decoder_vocab_size=None,
@@ -38,9 +49,7 @@ class Seq2SeqModel(nn.Module):
                  dialog_decoder_rnn_type='LSTM',
                  dialog_decoder_dropout_rate=0.5,
                  dialog_decoder_clipnorm=1.0,
-                 dialog_decoder_clipvalue=0.5,
                  dialog_decoder_max_length=32,
-                 dialog_decoder_bidirectional=True,
                  dialog_decoder_embedding=None,
                  dialog_decoder_pad_id=0,
                  dialog_decoder_eos_id=3,
@@ -49,7 +58,7 @@ class Seq2SeqModel(nn.Module):
                  device=None
                  ):
         # super init
-        super(Seq2SeqModel, self).__init__()
+        super(KnowledgeGroundedModel, self).__init__()
 
         '''Dialog encoder parameters'''
         self.dialog_encoder_vocab_size = dialog_encoder_vocab_size
@@ -60,10 +69,15 @@ class Seq2SeqModel(nn.Module):
         self.dialog_encoder_dropout_rate = dialog_encoder_dropout_rate
         self.dialog_encoder_max_length = dialog_encoder_max_length
         self.dialog_encoder_clipnorm = dialog_encoder_clipnorm
-        self.dialog_encoder_clipvalue = dialog_encoder_clipvalue
         self.dialog_encoder_bidirectional = dialog_encoder_bidirectional
         self.dialog_encoder_pad_id = dialog_encoder_pad_id
         self.dialog_encoder_tied = dialog_encoder_tied
+
+        '''facts encoder parameters'''
+        self.facts_embedding_size = facts_embedding_size
+        self.facts_vocab_size = facts_vocab_size
+        self.facts_max_length = facts_max_length
+        self.facts_dropout_rate = facts_dropout_rate
 
         '''Dialog decoder parameters'''
         self.dialog_decoder_vocab_size = dialog_decoder_vocab_size
@@ -74,8 +88,6 @@ class Seq2SeqModel(nn.Module):
         self.dialog_decoder_dropout_rate = dialog_decoder_dropout_rate
         self.dialog_decoder_max_length = dialog_decoder_max_length
         self.dialog_decoder_clipnorm = dialog_decoder_clipnorm
-        self.dialog_decoder_clipvalue = dialog_decoder_clipvalue
-        self.dialog_decoder_bidirectional = dialog_decoder_bidirectional
         self.dialog_decoder_pad_id = dialog_decoder_pad_id
         self.dialog_decoder_eos_id = dialog_decoder_eos_id
         self.dialog_decoder_attention_type = dialog_decoder_attention_type
@@ -94,19 +106,28 @@ class Seq2SeqModel(nn.Module):
             num_layers=self.dialog_encoder_num_layers,
             hidden_size=self.dialog_encoder_hidden_size,
             dropout=self.dialog_encoder_dropout_rate,
-            embedding=dialog_encoder_embedding
-        )
+            embedding=dialog_encoder_embedding)
+
+        # facts encoder
+        # mi = A * ri
+        self.facts_linearA = nn.Linear(self.facts_embedding_size,
+                                       self.dialog_decoder_hidden_size)
+        # ci = C * ri
+        self.facts_linearC = nn.Linear(self.facts_embedding_size,
+                                       self.dialog_decoder_hidden_size)
+
+        # softmax
+        self.facts_softmax = nn.SoftMax(dim=0)
 
         # Dialog Decoder with Attention
         self.dialog_decoder = StdRNNDecoder(
             rnn_type=self.dialog_decoder_rnn_type,
-            bidirectional_encoder=self.dialog_decoder_bidirectional,
+            bidirectional_encoder=self.dialog_encoder_bidirectional,
             num_layers=self.dialog_decoder_num_layers,
             hidden_size=self.dialog_decoder_hidden_size,
             dropout=self.dialog_decoder_dropout_rate,
             embedding=dialog_decoder_embedding,  # maybe replace by dialog_decoder_embedding
-            attn_type=self.dialog_decoder_attention_type
-        )
+            attn_type=self.dialog_decoder_attention_type)
 
         self.dialog_decoder_linear = nn.Linear(
             self.dialog_decoder_hidden_size, self.dialog_decoder_vocab_size)
@@ -132,13 +153,20 @@ class Seq2SeqModel(nn.Module):
     def forward(self,
                 dialog_encoder_inputs,  # LongTensor
                 dialog_encoder_inputs_length,
+                facts_inputs,
+                facts_inputs_length,
                 dialog_decoder_inputs,
-                dialog_decoder_inputs_length,
-                dialog_decoder_targets,
                 teacher_forcing_ratio=0.5,
                 batch_size=128
                 ):
-
+        """
+        Args:
+            - dialog_encoder_inputs: [max_length, batch_size]
+            - dialog_encoder_inputs_length: [max_length, batch_size]
+            - facts_inputs: [facts_size, facts_embedding_size]
+            - facts_inputs: [facts_inputs_length * len(facts_inputs)]
+            - dialog_decoder_inputs: [max_length, batch_size]
+        """
         # init, [-sqrt(3/hidden_size), sqrt(3/hidden_size)]
 
         dialog_encoder_state = self.dialog_encoder.init_hidden(
@@ -152,23 +180,28 @@ class Seq2SeqModel(nn.Module):
         )
 
         '''dialog_decoder forward'''
-        # tgt, memory_bank, state, memory_lengths=None
-        # dialog_decoder_state = RNNDecoderState(self.dialog_decoder_hidden_size, dialog_encoder_state)
+
         dialog_decoder_state = self.dialog_decoder.init_decoder_state(
             encoder_final=dialog_encoder_state)
-
         use_teacher_forcing = True if random.random() < teacher_forcing_ratio else False
 
-        dialog_decoder_outputs = torch.ones((self.dialog_decoder_max_length, \
-                                             batch_size, self.dialog_decoder_hidden_size), \
+        dialog_decoder_outputs = torch.ones((self.dialog_decoder_max_length,
+                                             batch_size, self.dialog_decoder_hidden_size),
                                             device=self.device) * self.dialog_decoder_pad_id
 
         dialog_decoder_attns_std = torch.zeros((self.dialog_decoder_max_length,
-                                            batch_size, self.dialog_decoder_max_length-2))
+                                                batch_size, self.dialog_decoder_max_length-2))
 
         if use_teacher_forcing:
             # Teacher forcing: Feed the target as the next input
             for di in range(self.dialog_decoder_max_length):
+                '''facts encoder forward'''
+                new_hidden_tuple = self.facts_forward(
+                    facts_inputs,
+                    dialog_decoder_state,
+                    batch_size)
+                dialog_decoder_state.update_state(new_hidden_tuple)
+
                 dialog_decoder_state, dialog_decoder_output, \
                     dialog_decoder_attn = self.dialog_decoder(
                         tgt=dialog_decoder_inputs[di].view(1, -1),
@@ -183,6 +216,12 @@ class Seq2SeqModel(nn.Module):
             # input
             dialog_decoder_input = dialog_decoder_inputs[0]
             for di in range(self.dialog_decoder_max_length):
+                '''facts encoder forward'''
+                new_hidden_tuple = self.facts_forward(
+                    facts_inputs,
+                    dialog_decoder_state,
+                    batch_size)
+                dialog_decoder_state.update_state(new_hidden_tuple)
                 dialog_decoder_state, dialog_decoder_output, \
                     dialog_decoder_attn = self.dialog_decoder(
                         tgt=dialog_decoder_input.view(1, -1),
@@ -198,16 +237,6 @@ class Seq2SeqModel(nn.Module):
                 if dialog_decoder_input[0].item() == self.dialog_decoder_eos_id:
                     break
 
-        """
-        dialog_decoder_state, dialog_decoder_outputs, \
-            dialog_decoder_attns_std = self.dialog_decoder(
-                tgt=dialog_decoder_inputs,
-                memory_bank=dialog_encoder_memory_bank,
-                state=dialog_decoder_state,
-                memory_lengths=dialog_encoder_inputs_length)
-
-        """
-
         # beam search  dialog_decoder_outputs -> [tgt_len x batch x hidden]
         dialog_decoder_outputs = self.dialog_decoder_linear(
             dialog_decoder_outputs)
@@ -219,12 +248,43 @@ class Seq2SeqModel(nn.Module):
         return ((dialog_encoder_state, dialog_encoder_memory_bank),
                 (dialog_decoder_state, dialog_decoder_outputs, dialog_decoder_attns_std))
 
+    def facts_forward(self,
+                      facts_inputs,
+                      dialog_decoder_state,
+                      batch_size):
+                # M
+        facts_M = self.facts_linearA(facts_inputs)
+        # C
+        facts_C = self.facts_lienarC(facts_inputs)
+        # softmax
+        hidden_tuple = dialog_decoder_state
+        # hidden_tuple is a tuple object
+        new_hidden_list = []
+        for hidden_state in hidden_tuple:
+            new_hidden_state = torch.zeros((self.dialog_decoder_num_layers,
+                                            batch_size,
+                                            self.dialog_decoder_hidden_size),
+                                           device=self.device)
+            for i in range(self.dialog_decoder_num_layers):
+                u = hidden_state[i]
+                tmpP = u @ facts_M.t()
+                P = self.facts_softmax(tmpP.view(-1))
+                # o
+                o = P.view(1, -1) * facts_C  # [1, hidden_size]
+                # u_
+                u_ = o + u
+                new_hidden_state[i] = u_.squeeze(0)
+            new_hidden_list.append(new_hidden_state)
+
+        new_hidden_tuple = tuple(new_hidden_list)
+        return new_hidden_tuple
+
     def evaluate(self,
-                dialog_encoder_inputs,  # LongTensor
-                dialog_encoder_inputs_length,
-                dialog_decoder_inputs,
-                batch_size=128
-                ):
+                 dialog_encoder_inputs,  # LongTensor
+                 dialog_encoder_inputs_length,
+                 dialog_decoder_inputs,
+                 batch_size=128
+                 ):
 
         dialog_encoder_state = self.dialog_encoder.init_hidden(
             batch_size, self.device)
@@ -240,12 +300,12 @@ class Seq2SeqModel(nn.Module):
         dialog_decoder_state = self.dialog_decoder.init_decoder_state(
             encoder_final=dialog_encoder_state)
 
-        dialog_decoder_outputs = torch.ones((self.dialog_decoder_max_length, \
-                                             batch_size, self.dialog_decoder_hidden_size), \
+        dialog_decoder_outputs = torch.ones((self.dialog_decoder_max_length,
+                                             batch_size, self.dialog_decoder_hidden_size),
                                             device=self.device) * self.dialog_decoder_pad_id
 
         dialog_decoder_attns_std = torch.zeros((self.dialog_decoder_max_length,
-                                            batch_size, self.dialog_decoder_max_length-2))
+                                                batch_size, self.dialog_decoder_max_length-2))
 
         dialog_decoder_input = dialog_decoder_inputs[0]
         for di in range(self.dialog_decoder_max_length):
@@ -257,7 +317,8 @@ class Seq2SeqModel(nn.Module):
                     memory_lengths=dialog_encoder_inputs_length
                 )
             dialog_decoder_outputs[di] = dialog_decoder_output.squeeze(0)
-            dialog_decoder_attns_std[di] = dialog_decoder_attn['std'].squeeze(0)
+            dialog_decoder_attns_std[di] = dialog_decoder_attn['std'].squeeze(
+                0)
             # greedy search
             dialog_decoder_input = torch.argmax(
                 dialog_decoder_output).detach().view(1, -1)
