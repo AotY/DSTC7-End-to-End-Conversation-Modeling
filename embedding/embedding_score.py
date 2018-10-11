fb404bd6ba531fe640f6425f88ca4ef8456290fd
AotY <694731929@qq.com>
2018-10-11 10:28:11 +0800

    indent

--- embedding/embedding_score.py
+++ embedding/embedding_score.py
@@ -3,114 +3,9 @@ from __future__ import division
 from __future__ import print_function
 
 import numpy as np
-from collections import Counter
-
-import gensim
 
 import torch
 import torch.nn.functional as F
-
-'''
-Get embedding score with tfidf weight
-'''
-
-
-def get_tfidf_embedding_score(vocab, gensim_model, tfidf, input_str, candidate_replies, stop_word_obj, lower=None,
-                              normal=False):
-    query_words = stop_word_obj.remove_words(input_str.strip().replace('\t', ' '))
-    query_len = len(query_words)
-    # to lower
-    if lower:
-        query_words = [word.lower() for word in query_words]
-
-    # init
-    query_tfidf_weights = np.array([round(1.0 / query_len, 5)] * query_len)
-
-    # freq
-    query_words_counter = Counter(query_words)
-
-    query_words_embedding = []
-    for idx, word in enumerate(query_words):
-        try:
-            word_embedding = gensim_model.wv[word]
-        except KeyError:
-            word_embedding = gensim_model.wv[vocab.unk]
-        query_words_embedding.append(word_embedding)
-
-        word_tfidf = (query_words_counter[word] / query_len * 1.0) * (tfidf.idf_dict.get(word, 0.0))
-        # print('query_word: {}, word_tfidf:    {}'.format(word, word_tfidf))
-
-        query_tfidf_weights[idx] = word_tfidf
-
-    # scalar
-    query_max_weight = np.max(query_tfidf_weights)
-    query_min_weight = np.min(query_tfidf_weights)
-    if query_min_weight != query_max_weight:
-        query_tfidf_weights = np.divide((query_tfidf_weights - query_min_weight),
-                                        (query_max_weight - query_min_weight) * 1.0)
-    else:
-        query_tfidf_weights = np.divide(query_tfidf_weights, np.sum(query_tfidf_weights))
-
-    query_tfidf_weight_vector = np.zeros_like(word_embedding, dtype=np.float64)
-
-    for word_embedding, weight in zip(query_words_embedding, query_tfidf_weights):
-        query_tfidf_weight_vector = np.add(query_tfidf_weight_vector, word_embedding * weight)
-
-    tfidf_matrix_candidate = []
-    for candidate_reply in candidate_replies:
-        candidate_words = stop_word_obj.remove_words(candidate_reply.strip().replace('\t', ' '))
-
-        # to lower
-        if lower:
-            candidate_words = [word.lower() for word in candidate_words]
-
-        candidate_len = len(candidate_words)
-
-        # init
-        candidate_tfidf_weights = np.array([round(1.0 / candidate_len, 5)] * candidate_len)
-
-        # freq
-        candidate_words_counter = Counter(candidate_words)
-
-        candidate_words_embedding = []
-        for idx, word in enumerate(candidate_words):
-            try:
-                word_embedding = gensim_model.wv[word]
-            except KeyError:
-                word_embedding = gensim_model.wv[vocab.unk]
-            candidate_words_embedding.append(word_embedding)
-
-            word_tfidf = (candidate_words_counter[word] / candidate_len) * (tfidf.idf_dict.get(word, 0.0))
-            # print('candidate_word: {}, tfidf:    {}'.format(word, word_tfidf))
-
-            candidate_tfidf_weights[idx] = word_tfidf
-
-        # scalar
-        candidate_max_weight = np.max(candidate_tfidf_weights)
-        candidate_min_weight = np.min(candidate_tfidf_weights)
-        if candidate_min_weight != candidate_max_weight:
-            candidate_tfidf_weights = np.divide((candidate_tfidf_weights - candidate_min_weight),
-                                                (candidate_max_weight - candidate_min_weight) * 1.0)
-        else:
-            candidate_tfidf_weights = np.divide(candidate_tfidf_weights, np.sum(candidate_tfidf_weights))
-
-        candidate_tfidf_weight_vector = np.zeros_like(word_embedding, dtype=np.float64)
-
-        for word_embedding, weight in zip(candidate_words_embedding, candidate_tfidf_weights):
-            candidate_tfidf_weight_vector = np.add(candidate_tfidf_weight_vector, word_embedding * weight)
-
-        tfidf_matrix_candidate.append(candidate_tfidf_weight_vector)
-
-    tfidf_matrix_candidate = np.array(tfidf_matrix_candidate)
-
-    score_vector = gensim_model.cosine_similarities(query_tfidf_weight_vector, tfidf_matrix_candidate)
-
-    # normalization
-    if normal:
-        score_vector = score_normalization(score_vector)
-
-    return score_vector
-
 
 '''
 Average:
@@ -128,8 +23,7 @@ def get_top_k_fact_average(encoder_embedding, fact_embedding, encoder_embedding_
     new_fact_inputs = torch.zeros((batch_size, top_k, fact_embedding_size), device=device)
     for bi in range(batch_size):
         cur_fact_input = fact_inputs[bi]
-        # cur_fact_input: [total_n, len()]
-        cur_fact_input_embedded_mean = torch.zeros((len(cur_part_fact), fact_embedding_size), device=device)
+        # cur_fact_input: [total_n, len()] cur_fact_input_embedded_mean = torch.zeros((len(cur_fact_input), fact_embedding_size), device=device)
         for ci, cur_part_fact in enumerate(cur_fact_input):
             cur_part_fact = torch.LongTensor(cur_part_fact, device=device)
             cur_part_fact_embedded = fact_embedding(cur_part_fact.view(1, -1))
