9190849eb665ca52717b837f5b4ff0e0e74a72b8
AotY <694731929@qq.com>
2018-10-11 18:20:51 +0800

    lstm

--- train_seq2seq_model.sh
+++ train_seq2seq_model.sh
@@ -8,7 +8,7 @@ python train_seq2seq_model.py \
     --dialog_encoder_embedding_size 300 \
     --dialog_encoder_hidden_size 300 \
     --dialog_encoder_num_layers 2 \
-    --dialog_encoder_rnn_type GRU \
+    --dialog_encoder_rnn_type LSTM \
     --dialog_encoder_dropout_probability 0.8 \
     --dialog_encoder_max_length 50 \
     --dialog_encoder_clipnorm 50.0 \
@@ -18,7 +18,7 @@ python train_seq2seq_model.py \
     --dialog_decoder_embedding_size 300 \
     --dialog_decoder_hidden_size 300 \
     --dialog_decoder_num_layers 2 \
-    --dialog_decoder_rnn_type GRU \
+    --dialog_decoder_rnn_type LSTM \
     --dialog_decoder_dropout_probability 0.8 \
     --dialog_decoder_max_length 50 \
     --dialog_decoder_clipnorm 50.0 \
