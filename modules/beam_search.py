b1f0136b9f3b6dd9e0dd22f78a2abf277460822c
AotY <694731929@qq.com>
2018-10-31 14:28:28 +0800

    beam search

--- modules/beam_search.py
+++ modules/beam_search.py
@@ -9,11 +9,11 @@ Beam Search
 """
 import operator
 from queue import PriorityQueue
-
 import torch
 import torch.nn as nn
 
-class Node(object):
+
+class BeamNode:
     def __init__(self,
                  previous_node,
                  hidden_state,
@@ -22,7 +22,7 @@ class Node(object):
                  length):
         """
         hidden_sate: dialogue_decoder_state
-        previous_node: previous Node
+        previous_node: previous BeamNode
         decoder_input:
         length: cur decoded length
         """
@@ -47,141 +47,151 @@ class Node(object):
             return self.length < self.length
     """
 
+
+"""
 class BeamSearch():
     def __init__(self, max_queue_size=1000):
         super(BeamSearch, self).__init__()
         self.max_queue_size = max_queue_size
 
-    def decode(self,
-                decoder,
-                c_encoder_outputs,
-                h_encoder_outputs,
-                decoder_hidden_state,
-                decoder_input,
-                batch_size,
-                beam_width,
-                best_n,
-                eosid,
-                r_max_len):
-        """
-        Args:
-            c_encoder_outputs: [len, batch, hidden_size]
-            decoder_hidden_state: [layers, batch, hidden_size]
-            decoder_input: [1, batch_size] * sosid
-        """
-        batch_utterances = []
-        for bi in range(batch_size):
-            c_encoder_outputs_bi = c_encoder_outputs[:, bi, :].unsqueeze(1)  # [max_length, 1, hidden_size]
+"""
 
-            if h_encoder_outputs is not None:
-                h_encoder_outputs_bi = h_encoder_outputs[:, bi, :].unsqueeze(1) # [num, 1, hidden_size]
-            else:
-                h_encoder_outputs_bi = None
 
-            init_decoder_hidden_state = decoder_hidden_state[:, bi, :].unsqueeze(1) #[layers, 1, hidden_size]
-            init_decoder_input = decoder_input[:, bi].unsqueeze(1) #[1, 1]
+def beam_search(
+    decoder,
+    c_encoder_outputs,
+    h_encoder_outputs,
+    decoder_hidden_state,
+    decoder_input,
+    batch_size,
+    beam_width,
+    best_n,
+    eosid,
+    r_max_len,
+    max_queue_size=1000):
 
-            # Number of sentence to generate
-            res_nodes = []
+    """
+    Args:
+        c_encoder_outputs: [len, batch, hidden_size]
+        decoder_hidden_state: [layers, batch, hidden_size]
+        decoder_input: [1, batch_size] * sosid
+    """
+    batch_utterances = []
+    for bi in range(batch_size):
+        c_encoder_outputs_bi = c_encoder_outputs[:, bi, :].unsqueeze(
+            1)  # [max_length, 1, hidden_size]
 
-            # starting node
-            init_node = Node(None, init_decoder_hidden_state, init_decoder_input, 0, 1)
-            node_queue = PriorityQueue(self.max_queue_size)
+        if h_encoder_outputs is not None:
+            h_encoder_outputs_bi = h_encoder_outputs[:, bi, :].unsqueeze(
+                1)  # [num, 1, hidden_size]
+        else:
+            h_encoder_outputs_bi = None
 
-            # start the queue
-            node_queue.put((-init_node.evaluate(), init_node))
+        init_decoder_hidden_state = decoder_hidden_state[:, bi, :].unsqueeze(
+            1)  # [layers, 1, hidden_size]
+        init_decoder_input = decoder_input[:, bi].unsqueeze(1)  # [1, 1]
 
-            q_size = 1
+        # Number of sentence to generate
+        res_nodes = []
 
-            # start beam search
-            while True:
-                # give up, when decoding takes too long
-                if q_size >= self.max_queue_size:
-                    break
+        # starting node
+        init_node = BeamNode(None, init_decoder_hidden_state,
+                             init_decoder_input, 0, 1)
 
-                # fetch the best node
-                cur_score, cur_node = node_queue.get()
+        node_queue = PriorityQueue(max_queue_size)
 
-                cur_decoder_input = cur_node.decoder_input
-                cur_decoder_hidden_state = cur_node.hidden_state
+        # start the queue
+        node_queue.put((-init_node.evaluate(), init_node))
 
-                # break
-                if (cur_decoder_input[0][0].item() == eosid or \
+        q_size = 1
+
+        # start beam search
+        while True:
+            # give up, when decoding takes too long
+            if q_size >= max_queue_size:
+                break
+
+            # fetch the best node
+            cur_score, cur_node = node_queue.get()
+
+            cur_decoder_input = cur_node.decoder_input
+            cur_decoder_hidden_state = cur_node.hidden_state
+
+            # break
+            if (cur_decoder_input[0][0].item() == eosid or
                     cur_node.length >= r_max_len) and \
                     cur_node.previous_node != None:
 
-                    res_nodes.append((cur_score, cur_node))
-                    # if we reached maximum
-                    if len(res_nodes) >= best_n:
-                        break
-                    else:
-                        continue
+                res_nodes.append((cur_score, cur_node))
+                # if we reached maximum
+                if len(res_nodes) >= best_n:
+                    break
+                else:
+                    continue
 
-                # decode for one step using decoder
-                decoder_output, next_decoder_hidden_state, _ = decoder(
-                    cur_decoder_input.contiguous(),
-                    cur_decoder_hidden_state.contiguous(),
-                    c_encoder_outputs_bi.contiguous(),
-                    h_encoder_outputs_bi
+            # decode for one step using decoder
+            decoder_output, next_decoder_hidden_state, _ = decoder(
+                cur_decoder_input.contiguous(),
+                cur_decoder_hidden_state.contiguous(),
+                c_encoder_outputs_bi.contiguous(),
+                h_encoder_outputs_bi
+            )
+
+            # put here real beam search of top
+            log_probs, indices = torch.topk(decoder_output, beam_width, dim=2)
+
+            next_nodes = []
+            count = 0
+            for i in range(beam_width):
+                next_decoder_input = indices[0, 0, i].view(-1, 1)  # [1, 1]
+                log_prob = log_probs[0, 0, i].item()
+
+                next_node = BeamNode(
+                    cur_node,
+                    next_decoder_hidden_state,
+                    next_decoder_input,
+                    cur_node.log_prob + log_prob,
+                    cur_node.length + 1
                 )
 
-                # put here real beam search of top
-                log_probs, indices = torch.topk(decoder_output, beam_width, dim=2)
+                next_score = - next_node.evaluate()
+                #  next_nodes.append((next_score, next_node))
+                try:
+                    node_queue.put((next_score, next_node))
+                    count += 1
+                except TypeError as e:
+                    print('next_score: %d' % next_score)
+                    print(next_node)
+                    continue
 
-                next_nodes = []
-                count = 0
-                for i in range(beam_width):
-                    next_decoder_input = indices[0, 0, i].view(-1, 1)  # [1, 1]
-                    log_prob = log_probs[0, 0, i].item()
+            """
+            for i in range(len(next_nodes)):
+                score, node = next_nodes[i]
+                node_queue.put((score, node))
+            """
 
-                    next_node = Node(
-                        cur_node,
-                        next_decoder_hidden_state,
-                        next_decoder_input,
-                        cur_node.log_prob + log_prob,
-                        cur_node.length + 1
-                    )
+            q_size += count
 
-                    next_score = - next_node.evaluate()
-                    #  next_nodes.append((next_score, next_node))
-                    try:
-                        node_queue.put((next_score, next_node))
-                        count += 1
-                    except TypeError as e:
-                        print('next_score: %d' % next_score)
-                        print(next_node)
-                        continue
+        # choose n_best paths, back trace them
+        if len(res_nodes) == 0:
+            res_nodes = [node_queue.get() for _ in range(best_n)]
 
-                """
-                for i in range(len(next_nodes)):
-                    score, node = next_nodes[i]
-                    node_queue.put((score, node))
-                """
+        utterances = []
+        for score, node in sorted(res_nodes, key=operator.itemgetter(0)):
+            utterance = []
+            utterance.append(node.decoder_input.item())
 
-                q_size += count
-
-            # choose n_best paths, back trace them
-            if len(res_nodes) == 0:
-                res_nodes = [node_queue.get() for _ in range(best_n)]
-
-            utterances = []
-            for score, node in sorted(res_nodes, key=operator.itemgetter(0)):
-                utterance = []
+            # back trace
+            while node.previous_node is not None:
+                node = node.previous_node
                 utterance.append(node.decoder_input.item())
 
-                # back trace
-                while node.previous_node is not None:
-                    node = node.previous_node
-                    utterance.append(node.decoder_input.item())
+            # reverse
+            utterance.reverse()
+            utterances.append(utterance)
 
-                # reverse
-                utterance.reverse()
-                utterances.append(utterance)
+        batch_utterances.append(utterances)
+        del node_queue
+        del res_nodes
 
-            batch_utterances.append(utterances)
-            del node_queue
-            del res_nodes
-
-        return batch_utterances
-
-
+    return batch_utterances
