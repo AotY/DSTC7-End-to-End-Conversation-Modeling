51e8a9617a601e3ad2d77e11a02527e1f95d977f
AotY <694731929@qq.com>
2018-11-02 18:13:07 +0800

    fact stats

--- misc/fact_stats.py
+++ misc/fact_stats.py
@@ -10,10 +10,16 @@
 Wikipedia: 6925
 
 Wikipedia contains table
-"""
 
+title_count: 8471
+wiki_count: 6925
+wiki ratio: 0.8175
+wiki_table_count: 5052
+table_ratio: 0.7295
+"""
 import os
 from tqdm import tqdm
+
 
 title_count = 0
 wiki_count = 0
@@ -27,6 +33,9 @@ maybe_table = False
 facts_path = './../data/facts.txt'
 with open(facts_path, 'r', encoding='utf-8') as f:
     for line in tqdm(f):
+        if not bool(line.rstrip()):
+            continue
+
         parts = line.split('\t')
 
         subreddit_name = parts[0]
@@ -47,30 +56,39 @@ with open(facts_path, 'r', encoding='utf-8') as f:
                 <p>
                 """
                 is_wiki = True
-                last_domain = domain_name
             else:
                 is_wiki = False
+
+            last_domain = domain_name
+            continue
 
         if is_wiki and domain_name == last_domain:
             if fact.find('jump to : navigation , search') != -1:
                 maybe_table = True
+                continue
 
         if is_wiki and domain_name == last_domain and maybe_table:
             if fact.find('<p>') != -1 or fact.find('<h2>') != -1:
-                if line_count > 1:
+                if line_count > 2:
                     wiki_table_count += 1
+                maybe_table = False
+                line_count = 0
             else:
                 line_count += 1
+                continue
 
         if domain_name != last_domain:
             is_wiki = False
             domain_name = ''
             maybe_table = False
+            line_count = 0
 
 
 print('title_count: %d' % title_count)
 print('wiki_count: %d' % wiki_count)
+print('wiki ratio: %.4f ' % (wiki_count / title_count))
 print('wiki_table_count: %d' % wiki_table_count)
+print('table_ratio: %.4f' % (wiki_table_count / wiki_count))
 
 
 
