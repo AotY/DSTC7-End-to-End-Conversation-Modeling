c7f9a4a512cc2be7198de3bf072d581b2c5b679f
AotY <694731929@qq.com>
2018-11-02 12:49:32 +0800

    beam search

--- modules/beam_search_original.py
+++ modules/beam_search_original.py
@@ -8,6 +8,7 @@
 Beam Search,
 Original method
 """
+import copy
 import torch
 
 
@@ -31,28 +32,27 @@ def beam_decode(
         decoder_hidden_state: [layers, batch, hidden_size]
         decoder_input: [1, batch_size] * sosid
     """
+
     batch_utterances = []
     for bi in range(batch_size):
         init_c_encoder_outputs = c_encoder_outputs[:, bi, :].unsqueeze(1).contiguous()  # [max_length, 1, hidden_size]
 
+        init_h_encoder_outputs = None
         if h_encoder_outputs is not None:
             init_h_encoder_outputs = h_encoder_outputs[:, bi, :].unsqueeze(1).contiguous()  # [num, 1, hidden_size]
-        else:
-            init_h_encoder_outputs = None
 
         init_decoder_hidden_state = decoder_hidden_state[:, bi, :].unsqueeze(1).contiguous()  # [layers, 1, hidden_size]
-        init_decoder_input = decoder_input[:, bi].unsqueeze(1).contiguous()  # [1, 1]
+        init_decoder_input = decoder_input[:, bi].view(1, -1).contiguous()  # [1, 1]
 
         next_decoder_input = None
-        next_hidden_state = None
         next_decoder_hidden_state = None
         next_c_encoder_outputs = None
         next_h_encoder_outputs = None
 
-        #  last_scores = [0] * beam_width
+        last_scores = [0] * beam_width
         cur_scores = [0] * beam_width
 
-        #  last_sentences = [[]] * beam_width
+        last_sentences = [[]] * beam_width
         cur_sentences = [[]] * beam_width
 
         res = []
@@ -69,7 +69,7 @@ def beam_decode(
                     init_decoder_hidden_state,
                     init_c_encoder_outputs,
                     init_h_encoder_outputs
-                ) # output: [1, 1, vocab_size], decoder_hidden_state: [num_layers, 1, hidden_size]
+                ) # output: [1, 1, vocab_size], hidden_sate: [num_layers, 1, hidden_size]
 
                 log_probs, indices = torch.topk(output, beam_width, dim=2) # [1, 1, beam_width]
 
@@ -103,45 +103,61 @@ def beam_decode(
                 output = output.view(-1).contiguous()
                 log_probs, indices = output.topk(cur_beam_width)
 
+                """
                 tmp_update = []
                 for index, prob in zip(indices.tolist(), log_probs.tolist()):
                     last_index = index // vocab_size
                     vocab_index = index % vocab_size
                     tmp_update.append((last_index, vocab_index, prob))
                 tmp_update = sorted(tmp_update, key=lambda item: item[0])
+                """
 
                 next_decoder_input = []
                 next_indices = []
+                remove_indices = []
 
-                for i, (last_index, vocab_index, prob) in enumerate(tmp_update):
+                for i, (index, prob) in zip(indices.tolist(), log_probs.tolist()):
+                    last_index = index // vocab_size
+                    vocab_index = index % vocab_size
+
+                    next_decoder_input.append(vocab_index)
+                    next_indices.append(i)
+
+                    cur_scores[i] = last_scores[last_index] + prob
+                    last_sentences[last_index].append(vocab_index)
+                    cur_sentences[i] = last_sentences[last_sentences].copy()
+
                     if vocab_index == eosid:
                         res.append((cur_scores[i] / len(cur_sentences[i]), cur_sentences[i]))
-                        cur_scores.remove(cur_scores[i])
-                        cur_sentences.remove(cur_sentences[i])
-                    else:
-                        next_decoder_input.append(vocab_index)
-                        next_indices.append(i)
+                        remove_indices.append(i)
 
-                        cur_scores[i] = cur_scores[last_index] + prob
-                        tmp_sentence = cur_sentences[last_index].copy()
-                        tmp_sentence.append(vocab_index)
-                        cur_sentences[i] = tmp_sentence
+                for i in remove_indices:
+                    next_decoder_input.remove(next_decoder_input[i])
+                    next_indices.remove(next_indices[i])
+                    cur_scores.remove(cur_scores[i])
+                    cur_sentences.remove(cur_sentences[i])
 
                 next_decoder_input = torch.tensor(next_decoder_input, dtype=torch.long, device=device)
-                next_decoder_input = next_decoder_input.view(1, -1)
+                next_decoder_input = next_decoder_input.view(1, -1) # [1, k]
 
                 if len(next_indices) != cur_beam_width:
                     next_indices = torch.tensor(next_indices, dtype=torch.long, device=device)
                     next_decoder_hidden_state = hidden_state.index_select(1, next_indices)
                     next_c_encoder_outputs = next_c_encoder_outputs.index_select(1, next_indices)
-
                 if next_h_encoder_outputs is not None:
                     next_h_encoder_outputs = next_h_encoder_outputs.index_select(1, next_indices)
+
+                # update
+                del last_scores
+                del last_sentences
+                last_scores = cur_scores.copy()
+                last_sentences = copy.deepcopy(cur_sentences)
 
         for score, sentence in zip(cur_scores, cur_sentences):
             res.append((score / len(sentence), sentence))
 
         best_n_sentences = [sentence for _, sentence in sorted(res, key=lambda item: item[0], reverse=True)][:best_n]
+        print('best_n_sentences[0]: {}'.format(best_n_sentences[0]))
         batch_utterances.append(best_n_sentences)
 
         del res
