ef3ce158aae0aa2946d7a5b2c5ac84f3d3679863
AotY <694731929@qq.com>
2018-11-08 14:19:19 +0800

    merge

--- misc/merge.sh
+++ misc/merge.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 python merge_convos_facts.py \
-    --convos_facts_folder_list /home/taoqing/Research/DSTC7/reddit/data-official-2011 /home/taoqing/Research/DSTC7/reddit/data-official-2012-13  \
+    --convos_facts_folder_list /home/taoqing/Research/DSTC7/reddit/data-official-2011 /home/taoqing/Research/DSTC7/reddit/data-official-2012-13  /home/taoqing/Research/DSTC7/reddit/2014 /home/taoqing/Research/DSTC7/reddit/2015-2017\
     --save_convos_path ./../data/train.convos.txt \
     --save_facts_path ./../data/train.facts.txt \
 
