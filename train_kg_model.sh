d399f0d24c8a9ea15be46b8174f7ed77fc47a77f
AotY <694731929@qq.com>
2018-11-08 11:14:19 +0800

    sh

--- train_kg_model.sh
+++ train_kg_model.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-export CUDA_VISIBLE_DEVICES=6
+export CUDA_VISIBLE_DEVICES=3
 
 python train_kg_model.py \
     --pair_path data/conversations_responses.pair.txt \
@@ -44,9 +44,9 @@ python train_kg_model.py \
     --test_split 0.06 \
     --start_epoch 1 \
     --task train \
-    --model_type seq2seq \
+    --model_type kg \
     --share_embedding \
-    --checkpoint models/checkpoint.epoch-1_seq2seq_4_hred.pth
+    # --checkpoint models/checkpoint.epoch-1_seq2seq_4_hred.pth
     # --pre_trained_embedding data/fasttext_vec_for_vocab_seq2seq.60004.300d.npy \
 
 /
