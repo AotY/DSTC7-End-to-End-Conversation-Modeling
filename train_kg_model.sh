12da6291d8f44fadca30c0302c98df9bc2ab8a96
AotY <694731929@qq.com>
2018-11-12 18:23:33 +0800

    self attn

--- train_kg_model.sh
+++ train_kg_model.sh
@@ -4,7 +4,7 @@ export CUDA_VISIBLE_DEVICES=3
 python train_kg_model.py \
     --pair_path data/conversations_responses.pair.txt \
     --save_path data/ \
-    --vocab_path data/vocab_word2idx_seq2seq.80004.dict \
+    --vocab_path data/vocab_word2idx_seq2seq.60004.dict \
     --turn_num 4 \
     --min_turn 2 \
     --turn_type self_attn \
