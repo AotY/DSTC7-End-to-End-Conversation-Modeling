69172c3742bd9de2e94306d329d3b5d4be7aeafe
AotY <694731929@qq.com>
2018-11-12 15:01:35 +0800

    transformer

---
+++ modules/transformer/__init__.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright © 2018 LeonTao
+#
+# Distributed under terms of the MIT license.
+
+import transformer.modules
+import transformer.layers
+import transformer.sub_layers
+import transformer.models
+import transformer.sdqa
+import transformer.utils
+
+__all__ = [
+    transformer.modules, 
+    transformer.layers,
+    transformer.sub_layers,
+    transformer.models,
+    transformer.sdqa
+]
+
+
+
