2b68a428c3daa1ef56457a014b6e8b887e77d402
AotY <694731929@qq.com>
2018-11-21 15:57:54 +0800

    embedding

--- modules/luong_attn_decoder.py
+++ modules/luong_attn_decoder.py
@@ -7,7 +7,6 @@
 """
 
 """
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -49,7 +48,14 @@ class LuongAttnDecoder(nn.Module):
         self.h_attn = Attention(hidden_size)
 
         # f_attn
-        self.f_attn = Attention(hidden_size)
+        #  self.f_attn = Attention(hidden_size)
+        self.f_linearA = nn.Linear(self.embedding_size,
+                                   self.hidden_size + latent_size)
+
+        self.f_linearC = nn.Linear(self.embedding_size,
+                                   self.hidden_size + latent_size)
+        init_linear_wt(self.f_linearA)
+        init_linear_wt(self.f_linearC)
 
         self.rnn = rnn_factory(
             rnn_type,
@@ -65,7 +71,8 @@ class LuongAttnDecoder(nn.Module):
             init_gru_orth(self.rnn)
 
         if latent_size > 0:
-            self.latent_linear = nn.Linear(hidden_size + latent_size, hidden_size)
+            self.latent_linear = nn.Linear(
+                hidden_size + latent_size, hidden_size)
             init_linear_wt(self.latent_linear)
 
         # linear
@@ -89,15 +96,15 @@ class LuongAttnDecoder(nn.Module):
                 f_encoder_lengths=None,
                 z=None):
         '''
-		Args:
-			inputs: [1, batch_size]
-			hidden_state: [num_layers, batch_size, hidden_size]
+                Args:
+                        inputs: [1, batch_size]
+                        hidden_state: [num_layers, batch_size, hidden_size]
             inputs_length: [batch_size, ] or [1, ]
 
-			h_encoder_outputs: [turn_num, batch_size, hidden_size] or [1, batch_size, hidden_size]
+                        h_encoder_outputs: [turn_num, batch_size, hidden_size] or [1, batch_size, hidden_size]
             h_encoder_lengths: [batch_size]
 
-			f_encoder_outputs: [turn_num, batch_size, hidden_size] or [1, batch_size, hidden_size]
+                        f_encoder_outputs: [turn_num, batch_size, hidden_size] or [1, batch_size, hidden_size]
             f_encoder_lengths: [batch_size]
 
             z: for latent variable model. [num_layers, batch_size, latent_size]
@@ -109,7 +116,8 @@ class LuongAttnDecoder(nn.Module):
 
         if inputs_length is not None:
             # sort inputs_length
-            inputs_length, sorted_indexes = torch.sort(inputs_length, dim=0, descending=True)
+            inputs_length, sorted_indexes = torch.sort(
+                inputs_length, dim=0, descending=True)
             # restore to original indexes
             _, restore_indexes = torch.sort(sorted_indexes, dim=0)
 
@@ -120,7 +128,8 @@ class LuongAttnDecoder(nn.Module):
         embedded = self.dropout(embedded)
 
         if inputs_length is not None:
-            embedded = nn.utils.rnn.pack_padded_sequence(embedded, inputs_length)
+            embedded = nn.utils.rnn.pack_padded_sequence(
+                embedded, inputs_length)
 
         # Get current hidden state from inputs word and last hidden state
         if z is not None:
@@ -130,16 +139,19 @@ class LuongAttnDecoder(nn.Module):
 
         if inputs_length is not None:
             output, _ = nn.utils.rnn.pad_packed_sequence(output)
-            output = output.transpose(0, 1)[restore_indexes].transpose(0, 1).contiguous()
+            output = output.transpose(
+                0, 1)[restore_indexes].transpose(0, 1).contiguous()
 
         #  print('output: ', output)
 
         if h_encoder_outputs is not None and h_encoder_lengths is not None:
-            output, h_attn_weights = self.h_attn(output, h_encoder_outputs, h_encoder_lengths)
+            output, h_attn_weights = self.h_attn(
+                output, h_encoder_outputs, h_encoder_lengths)
             #  print('h_output: ', h_output)
 
-        if f_encoder_outputs is not None and f_encoder_lengths is not None:
-            output, f_attn_weights = self.f_attn(output, f_encoder_outputs, f_encoder_lengths)
+        if f_encoder_outputs is not None:
+            #  output, f_attn_weights = self.f_attn(output, f_encoder_outputs, f_encoder_lengths)
+            output = self.f_forward(output, f_encoder_outputs)
             #  print('output: ', output)
 
         if self.latent_size > 0:
@@ -152,3 +164,24 @@ class LuongAttnDecoder(nn.Module):
         output = F.log_softmax(output, dim=2)
 
         return output, hidden_state, None
+
+    def f_forward(self, output, f_encoder_outputs):
+        # M [batch_size, topk, hidden_size]
+        fM = self.f_linearA(f_encoder_outputs)
+
+        # C [batch_size, topk, hidden_size]
+        fC = self.f_linearC(f_encoder_outputs)
+
+        # [batch_size, num_layers, topk]
+        tmpP = torch.bmm(output.transpose(0, 1), fM.transpose(1, 2))
+
+        P = F.softmax(tmpP, dim=2)
+
+        o = torch.bmm(P, fC)  # [batch_size, num_layers, hidden_size]
+
+        u_ = torch.add(o, output.transpose(0, 1))
+
+        # [num_layers, batch_size, hidden_size]
+        u_ = u_.transpose(0, 1).contiguous()
+
+        return u_
