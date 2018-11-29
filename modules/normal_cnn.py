4e75ef119f282f2a1b38c4e35eba524ffdd25adf
AotY <694731929@qq.com>
2018-11-29 10:32:57 +0800

    cnn

--- modules/normal_cnn.py
+++ modules/normal_cnn.py
@@ -30,31 +30,22 @@ class NormalCNN(nn.Module):
         self.dropout = nn.Dropout(config.dropout)
 
         self.conv2ds = nn.ModuleList()
+
         self.bn2s = nn.ModuleList()
-        self.maxpool2ds = nn.ModuleList()
 
-        # conv2d
-        kernel_sizes = [(3, 512), (3, 512), (4, 256),
-                        (4, 128), (5, 256), (4, 512)]
-        channels = [512, 256, 128, 256, 512, 1024]
 
-        for channel, kernel_size in zip(channels, kernel_sizes):
+        # conv2d 120 -> 1
+        kernel_sizes = [(4, 512), (3, 512), (3, 256), (2, 256), (4, 512)]
+        channels = [512, 256, 256, 512, 1024]
+        strides = [(2, 1), (2, 1), (2, 1), (2, 1), (1, 1)]
+
+        for channel, kernel_size, stride in zip(channels, kernel_sizes, strides):
             self.conv2ds.append(
                 nn.Conv2d(
                     in_channels=1,
                     out_channels=channel,
                     kernel_size=kernel_size,
-                    stride=1
-                )
-            )
-
-        kernel_sizes = [(3, 1), (3, 1), (4, 1),
-                        (4, 1), (4, 1), (5, 1)]
-        for kernel_size in kernel_sizes:
-            self.maxpool2ds.append(
-                nn.MaxPool2d(
-                    kernel_size=kernel_size,
-                    stride=1
+                    stride=stride
                 )
             )
 
@@ -63,8 +54,9 @@ class NormalCNN(nn.Module):
                 nn.BatchNorm2d(channel)
             )
 
+        self. maxpool2d = nn.MaxPool2d(kernel_size=(4, 1))
 
-        self.out_linear = nn.Linear(1024, config.hidden_size)
+        self.out_linear = nn.Linear(channels[-1], config.hidden_size)
         init_linear_wt(self.out_linear)
 
     def forward(self, inputs, lengths=None):
@@ -74,8 +66,7 @@ class NormalCNN(nn.Module):
         return:
             [1, batch_size, hidden_size]
         """
-        embedded = self.embedding(
-            inputs)  # [max_len, batch_size, embedding_size]
+        embedded = self.embedding(inputs)  # [max_len, batch_size, embedding_size]
         embedded = self.dropout(embedded)
 
         # [batch_size, 1, max_len, embedding_size]
@@ -84,18 +75,18 @@ class NormalCNN(nn.Module):
 
         # conv
         output = embedded
-        for conv2d, bn2, maxpool2d in zip(self.conv2ds, self.bn2s, self.maxpool2ds):
+        for conv2d, bn2 in zip(self.conv2ds, self.bn2s):
             output = conv2d(output)
             output = bn2(output)
             output = F.relu(output)
-            output = maxpool2d(output)
             output = output.transpose(1, 3)
+            #  print('output: ', output.shape)
 
         # [batch_size, 1, 1, 1024]
+        output = self.maxpool2d(output)
 
+        #  print('cnn output: ', output.shape)
         output = output.squeeze(2).transpose(0, 1)
         output = self.out_linear(output)  # [1, batch_size, hidden_size]
-        #  print('output: ', output.shape)
 
         return output, None
-
