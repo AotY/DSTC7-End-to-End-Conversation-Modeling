87ae6cf44780ecd180405dda48b72a77258892e7
AotY <694731929@qq.com>
2018-12-10 17:42:45 +0800

    re clean

--- misc/re_clean.py
+++ misc/re_clean.py
@@ -6,23 +6,36 @@
 
 
 import re
+import string
 import argparse
 from tqdm import tqdm
+from nltk import ngrams
 from collections import Counter
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('--pair_path', type=str, default='../data/train.pseudo_convos.txt')
-parser.add_argument('--pair_save_path', type=str, default='../data/train.convos.txt')
+parser.add_argument('--pseudo_convos_path', type=str, default='../data/pseudo_convos.txt')
+parser.add_argument('--train_convos_path', type=str, default='../data/train.convos.txt')
 
-parser.add_argument('--fact_path', type=str, default='../data/train.pseudo_facts.txt')
-parser.add_argument('--fact_save_path', type=str, default='../data/train.facts.txt')
+parser.add_argument('--pseudo_facts_path', type=str, default='../data/pseudo_facts.txt')
+parser.add_argument('--train_facts_path', type=str, default='../data/train.facts.txt')
 
 parser.add_argument('--vocab_freq_path', type=str, default='./vocab_freq.txt')
 args = parser.parse_args()
 
+punc_regex = re.compile('[%s]' % re.escape(string.punctuation.replace('_', '')))
 
 def clean_number_url(text):
+    text = text.replace('( )', '')
+    text = text.replace('[ ]', '')
+    text = text.replace('{ }', '')
+    text = text.replace("' '", '')
+    text = text.replace('" "', '')
+    text = text.replace('. .', '.')
+    text = text.replace(', ,', ',')
+    text = text.replace(': :', ':')
+    text = text.replace('; ;', ';')
+
     text = text.replace('( __number__ )', '__number__')
     text = text.replace('( __url__ )', '__url__')
 
@@ -44,14 +57,30 @@ def clean_number_url(text):
     text = text.replace('__url __', ' __url__ ')
     return text
 
-def clean_repeat(text):
+def clean_repeat(text, max_ngram=4):
+    tmp_text = punc_regex.sub('', text)
+    text_ngrams = ngrams(tmp_text.split(), max_ngram)
+    for words in text_ngrams:
+        if len(set(words)) == 1:
+            return ''
+
+    words = []
+    for i, word in enumerate(text.split()):
+        if i == 0:
+            words.append(word)
+        else:
+            if word == words[i - 1]:
+                continue
+            else:
+                words.append(word)
+    return ' '.join(words)
 
 
 def main():
     freq_dict = Counter()
     # read pair
-    pair_save_f = open(args.pair_save_path, 'w')
-    with open(args.pair_path, 'r', encoding='utf-8') as f:
+    pair_save_f = open(args.train_convos_path, 'w')
+    with open(args.pseudo_convos_path, 'r', encoding='utf-8') as f:
         for line in tqdm(f):
             line = line.rstrip()
             if not bool(line):
@@ -67,23 +96,29 @@ def main():
             if not bool(query) or not bool(response):
                 continue
 
-            if len(context.split()) != 0:
+            if len(context.split()) > 0:
                 context = clean_number_url(context)
-                freq_dict.update(context.split())
+                context = clean_repeat(context)
+                if len(context.split()) > 0:
+                    freq_dict.update(context.split())
 
             query = clean_number_url(query)
-            freq_dict.update(query.split())
+            query = clean_repeat(query)
+            if len(query.split()) > 0:
+                freq_dict.update(query.split())
 
             response = clean_number_url(response)
-            freq_dict.update(response.split())
+            response = clean_repeat(response)
+            if len(response.split()) > 0:
+                freq_dict.update(response.split())
 
             pair_save_f.write('%s SPLIT %s SPLIT %s SPLIT %s SPLIT %s SPLIT %s SPLIT %s SPLIT %s\n' % \
                     (subreddit_name, conversation_id, context, query, response, hash_value, score, turn))
 
     pair_save_f.close()
 
-    fact_save_f = open(args.fact_save_path, 'w')
-    with open(args.fact_path, 'r', encoding='utf-8') as f:
+    fact_save_f = open(args.train_facts_path, 'w')
+    with open(args.pseudo_facts_path, 'r', encoding='utf-8') as f:
         for line in tqdm(f):
             line = line.rstrip()
             if not bool(line):
