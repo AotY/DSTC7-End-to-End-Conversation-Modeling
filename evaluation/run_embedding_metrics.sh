021b7f90b4c21a647ab29325159f63872e35b2d5
AotY <694731929@qq.com>
2018-12-13 18:15:41 +0800

    none, concat

--- evaluation/run_embedding_metrics.sh
+++ evaluation/run_embedding_metrics.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 #
 # run_embedding_metrics.sh
-# Copyright (C) 2018 LeonTao
-#
+# Copyright (C) 2018 LeonTao #
 # Distributed under terms of the MIT license.
 #
 
@@ -10,6 +9,6 @@
 python metrics/embedding_metrics.py \
     --embeddings ~/Research/data/GoogleNews-vectors-negative300.bin \
     --ground_truth ../data/ground_truth/1_4.txt \
-    --predicted ../data/predicted/none_3_1_4.txt \
+    --predicted ../data/predicted/concat_smoothing_1_1_4_2018_12_13_13:04.txt \
 
 /
