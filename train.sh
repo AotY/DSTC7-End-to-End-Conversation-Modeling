d74018890482afefaab2ad63c15d7a372ae4402b
AotY <694731929@qq.com>
2018-12-14 13:45:55 +0800

    qc_seq_h_attn

--- train.sh
+++ train.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # export CUDA_LAUNCH_BLOCKING=1
-export CUDA_VISIBLE_DEVICES=6
+export CUDA_VISIBLE_DEVICES=7
 
 python train.py \
     --pair_path data/train.convos.txt \
@@ -8,7 +8,7 @@ python train.py \
     --vocab_path data/vocab_word2idx.60004.dict \
     --c_max 3 \
     --c_min 1 \
-    --enc_type qc_seq_h \
+    --enc_type qc_seq_h_attn \
     --rnn_type GRU \
     --embedding_size 512 \
     --hidden_size 512 \
