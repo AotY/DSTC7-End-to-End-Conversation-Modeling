8c1986c1b6d9df5406fdaf660b3bcbf36ea3cf23
AotY <694731929@qq.com>
2018-12-19 11:25:36 +0800

    preprocess, dataset, q, qc

--- preprocess.sh
+++ preprocess.sh
@@ -3,13 +3,13 @@
 python misc/preprocess.py \
     --raw_convos_path ./data/raw.convos.txt \
     --raw_facts_path ./data/raw.facts.txt \
-    --train_convos_path ./data/train.convos.txt \
-    --train_facts_path ./data/train.facts.txt \
+    --train_convos_path ./data/train2.convos.txt \
+    --train_facts_path ./data/train2.facts.txt \
     --freq_save_path ./data/word.freq.txt \
     --save_path ./data \
     --min_len 3 \
-    --c_max_len 220 \
-    --q_max_len 270 \
-    --r_max_len 150 \
-    --f_max_len 500
+    --c_max_len 230 \
+    --q_max_len 280 \
+    --r_max_len 160 \
+    --f_max_len 510
 /
