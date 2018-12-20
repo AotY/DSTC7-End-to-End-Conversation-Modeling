92ff042910c8974cff0c672f630b40695c5c5d7d
AotY <694731929@qq.com>
2018-12-20 15:13:14 +0800

    sh

--- train.sh
+++ train.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 # export CUDA_LAUNCH_BLOCKING=1
-export CUDA_VISIBLE_DEVICES=3
+export CUDA_VISIBLE_DEVICES=6
 
 python train.py \
     --pair_path data/train2.convos.txt \
     --save_path data/ \
     --vocab_path data/vocab_word2idx.60004.dict \
     --c_max 3 \
-    --c_min 1 \
-    --enc_type qc \
+    --c_min 0 \
+    --enc_type q \
     --rnn_type GRU \
     --embedding_size 512 \
     --hidden_size 512 \
