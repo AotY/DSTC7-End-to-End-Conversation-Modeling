947d977758c90440ca243b465e96579c80f2e44b
AotY <694731929@qq.com>
2018-12-20 11:25:23 +0800

    dataset, sh

--- train.sh
+++ train.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 # export CUDA_LAUNCH_BLOCKING=1
-export CUDA_VISIBLE_DEVICES=5
+export CUDA_VISIBLE_DEVICES=6
 
 python train.py \
     --pair_path data/train2.convos.txt \
     --save_path data/ \
-    --vocab_path data/vocab_word2idx.40004.dict \
+    --vocab_path data/vocab_word2idx.60004.dict \
     --c_max 3 \
     --c_min 1 \
-    --enc_type qc \
+    --enc_type q \
     --rnn_type GRU \
     --embedding_size 512 \
     --hidden_size 512 \
@@ -20,7 +20,7 @@ python train.py \
     --inner_hidden_size 1024 \
     --k_size 64 \
     --v_size 64 \
-    --dropout 0.2 \
+    --dropout 0.0 \
     --bidirectional \
     --tied \
     --decode_type beam_search \
@@ -32,7 +32,7 @@ python train.py \
     --beam_size 8 \
     --best_n 3 \
     --f_topk 15 \
-    --lr 0.0005 \
+    --lr 0.001 \
     --max_grad_norm 5.0 \
     --epochs 25 \
     --batch_size 128 \
