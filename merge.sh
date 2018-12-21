171ff7651dab4e6a8d5f811a6056c87d356497d6
AotY <694731929@qq.com>
2018-12-21 13:05:16 +0800

    merge

---
+++ merge.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+python misc/merge_convos_facts.py \
+    --data_dir ~/Research/DSTC7/reddit \
+    --save_convos_path ./data/raw.2.convos.txt \
+    --save_facts_path ./data/raw.2.facts.txt \
+
+
+/
