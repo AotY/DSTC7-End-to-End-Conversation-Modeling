4a563267c252b7e3c767988c794ae5879094e0eb
AotY <694731929@qq.com>
2018-12-22 12:36:23 +0800

    train

--- train.sh
+++ train.sh
@@ -4,7 +4,7 @@ export CUDA_VISIBLE_DEVICES=6
 
 python train.py \
     --save_path data/ \
-    --convos_path ./data/cleaned.2.convos.txt.simpler \
+    --convos_path ./data/cleaned.2.convos.txt \
     --vocab_path data/vocab_word2idx.2.60004.dict \
     --c_max 3 \
     --c_min 1 \
